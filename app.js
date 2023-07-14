18628a0795fc3b83427d4cdef7079f0124e6af3a
dennzlak <134721688+dennzlak@users.noreply.github.com>
2023-07-14 12:30:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
+// made by dennzlak on github
+
+// to run this, you must have node.js
+// enter ' node app.js ' to run the program
+// change the 'y2mate.is' to any website you used
+
+
 const fs = require('fs');
 console.log(' MADE BY dennzlak ON GITHUB')
 console.log('https://github.com/dennzlak')
 
-// to run this, you must have node.js
-// enter ' node app.js ' to run the program
-// made by dennzlak on github
 
 const testFolder = 'YOUR MUSIC FOLDER GOES HERE';
-const phrase = 'PHRASE TO REMOVE GOES HERE (EG. Y2MATE, Y2META)';
 
 
 fs.readdir(testFolder, (err, files) => {
   files.forEach(file => {
-    if (file.startsWith(phrase)) {
-        var newname = file.replace(phrase, '')
+    if (file.startsWith('y2mate.is - ') {
+        var newname = file.replace('y2mate.is - ', '')
         var fileLocation = testFolder +'/'+file
         var newestName = testFolder + '/' + newname
         fs.renameSync(fileLocation, newestName)
