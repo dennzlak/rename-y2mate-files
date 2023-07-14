0cae9bdd3eadb666c78d77872fbe095e9f5b6a06
dennzlak <134721688+dennzlak@users.noreply.github.com>
2023-07-14 12:27:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
+const fs = require('fs');
+console.log(' MADE BY dennzlak ON GITHUB')
+console.log('https://github.com/dennzlak')
+
 // to run this, you must have node.js
 // enter ' node app.js ' to run the program
 // made by dennzlak on github
 
-
-console.log(' MADE BY dennzlak ON GITHUB')
-console.log('https://github.com/dennzlak')
-
 const testFolder = 'YOUR MUSIC FOLDER GOES HERE';
 const phrase = 'PHRASE TO REMOVE GOES HERE (EG. Y2MATE, Y2META)';
 
-const fs = require('fs');
 
 fs.readdir(testFolder, (err, files) => {
   files.forEach(file => {
